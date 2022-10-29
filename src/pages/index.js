29439d9698c0cbed801257bfd2b88ef43714e4a9
s128804 <jafar.sabaouni@student.ap.be>
2022-10-29 19:26:28 +0200

    Gatbsy Plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
+    <main>
       <Layout pageTitle="Welcome to Inghelbrecht Agency!">
       <p>Lorem ipsum</p>
+      <StaticImage
+        alt="a ape"
+        src="../images/ape.jpg"
+      />
       </Layout>
+    </main>
   )
 }
 
